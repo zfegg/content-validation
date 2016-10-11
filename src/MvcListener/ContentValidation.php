e79fff6e2879076482369cfb33dbd104bbbb2383
xiemaomao520@163.com <xiemaomao520@163.com>
2016-10-11 22:54:46 +0800

    1

--- src/MvcListener/ContentValidation.php
+++ src/MvcListener/ContentValidation.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Zfegg\MvcListener;
+
 use Zend\EventManager\AbstractListenerAggregate;
 use Zend\EventManager\EventManagerInterface;
 use Zend\Mvc\MvcEvent;
@@ -26,7 +27,23 @@ class ContentValidation extends AbstractListenerAggregate
     {
         $events->attach(MvcEvent::EVENT_DISPATCH, [$this, 'validation'], $priority);
         $events->attach(MvcEvent::EVENT_DISPATCH, [$this, 'validation'], $priority);
-        $events->attach(self::EVENT_VALIDATE_INVALID, [$this, 'onInvalid'], $priority);
+        $events->attach(self::EVENT_VALIDATE_INVALID, [$this, 'onInvalid'], -$priority);
+    }
+
+    public function onInvalid(MvcEvent $e)
+    {
+        /** @var \Zend\Http\PhpEnvironment\Response $response */
+        $response = $e->getResponse();
+
+        $response->setStatusCode(422);
+        $response->getHeaders()->addHeaderLine('Content-Type', 'application/json');
+        $response->setContent(json_encode([
+            'status' => 422,
+            'detail' => 'Failed Validation',
+            'validation_messages' => $e->getParam(self::INPUT_FILTER_NAME)->getMessages()
+        ]));
+
+        return $response;
     }
 
     public function validation(MvcEvent $e)
@@ -61,7 +78,7 @@ class ContentValidation extends AbstractListenerAggregate
         $e->setName(self::EVENT_VALIDATE_PREPARE);
         $events = $e->getApplication()->getEventManager();
         $results = $events->triggerEventUntil(function ($result) {
-            return is_array($result);
+            return is_array($result) || $result instanceof ResponseInterface;
         }, $e);
 
         $last = $results->last();
@@ -79,7 +96,10 @@ class ContentValidation extends AbstractListenerAggregate
             $results = $events->triggerEventUntil(function ($result) {
                 return $result instanceof ResponseInterface;
             }, $e);
-        }
 
+            if (($last = $results->last()) && $last instanceof ResponseInterface) {
+                return $last;
+            }
+        }
     }
 }
--- src/Psr7Middleware/ContentValidation.php
+++ src/Psr7Middleware/ContentValidation.php
@@ -5,6 +5,7 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Zend\InputFilter\InputFilterPluginManager;
 use Zend\ServiceManager\ServiceManager;
+use Zfegg\ContentValidation\ContentValidationTrait;
 
 
 /**
@@ -13,57 +14,12 @@ use Zend\ServiceManager\ServiceManager;
  */
 class ContentValidation
 {
-
-    protected $inputFilterManager;
-
-    protected $invalidHandler;
+    use ContentValidationTrait;
 
     public function __construct(array $options = [])
     {
 
     }
-
-    /**
-     * @return InputFilterPluginManager
-     */
-    public function getInputFilterManager()
-    {
-        if (null === $this->inputFilterManager) {
-            $this->inputFilterManager = new InputFilterPluginManager(new ServiceManager());
-        }
-
-        return $this->inputFilterManager;
-    }
-
-    /**
-     * @param InputFilterPluginManager $inputFilterManager
-     * @return $this
-     */
-    public function setInputFilterManager(InputFilterPluginManager $inputFilterManager)
-    {
-        $this->inputFilterManager = $inputFilterManager;
-        return $this;
-    }
-
-    /**
-     *
-     * @return callable
-     */
-    public function getInvalidHandler()
-    {
-        return $this->invalidHandler;
-    }
-
-    /**
-     * @param callable $invalidHandler
-     * @return $this
-     */
-    public function setInvalidHandler(callable $invalidHandler)
-    {
-        $this->invalidHandler = $invalidHandler;
-        return $this;
-    }
-
 
     public function __invoke(ServerRequestInterface $request, ResponseInterface $response, callable $next)
     {
